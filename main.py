ce9aa2606920d0d1f1a90542a2168eb915e65c63
Veronica-Void <vvoid8075@gmail.com>
2024-04-15 09:55:04 -0700

    added png and edited base.html

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request, redirect  # imported flask
 from sqlalchemy import create_engine, text
 
-c_str = "mysql://root:MySQL8090@localhost/database"
+c_str = "mysql://root:MySQL8090@localhost/bank_app"
 engine = create_engine(c_str, echo=True)
 connection = engine.connect()
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello"
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
