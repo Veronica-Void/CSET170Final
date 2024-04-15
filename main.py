3f9eeeda4e32a8abc3443346022ee1377c039151
Veronica-Void <vvoid8075@gmail.com>
2024-04-15 08:46:28 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect  # imported flask
+from sqlalchemy import create_engine, text
+
+c_str = "mysql://root:password@localhost/database"
+engine = create_engine(c_str, echo=True)
+connection = engine.connect()
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello"
+
+if __name__ == '__main__':
+    app.run(debug=True)
